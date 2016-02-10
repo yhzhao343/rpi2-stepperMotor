dca0648add5b82645bd7c75fb892ddb258bac327
yhzhao343 <yhzhao343@gmail.com>
2016-02-09 22:54:46 -0500

    f'

--- q-stepper-motor.js
+++ q-stepper-motor.js
@@ -14,12 +14,11 @@ function stepperMotor(motorPins) {
                   [0,0,1,1],
                   [0,0,0,1],
                   [1,0,0,1]];
-    this.init();
+    this.init().then(this.go);
 }
 
-stepperMotor.prototype.init = function() {
-    var setupFuncArray = this.funcArrayGen(this.pinSetupFuncGen(this));
-
+stepperMotor.prototype.init = function init() {
+    return Q.all(this.funcArrayGen(this.pinSetupFuncGen(this)));
 }
 stepperMotor.prototype.pinSetupFuncGen = function pinSetupFuncGen(thisP) {
     var mygpio = gpio;
@@ -38,4 +37,27 @@ stepperMotor.prototype.funcArrayGen = function funcArrayGen(func) {
         result.push(Q.denodeify(func(i)));
     }
     return result;
+}
+
+stepperMotor.prototype.go = function go() {
+    setInterval(this.step(this), this.velocity)
+}
+
+stepperMotor.prototype.step = function(thisPointer) {
+    switch(thisPointer.runStatus) {
+        case 1:
+        case 2:
+        case -1:
+        case -2:
+            thisPointer.cycleNdx += thisPointer.runStatus;
+            thisPointer.cycleNdx = thisPointer.cycleNdx > 7 ? thisPointer.cycleNdx - 8 : thisPointer.cycleNdx;
+            thisPointer.cycleNdx = thisPointer.cycleNdx < 0 ? thisPointer.cycleNdx + 8 : thisPointer.cycleNdx;
+            var val2Write = thisP.cycle[thisP.cycleNdx];
+            for (var i = 0; i < thisP.motorPins.length; i++) {
+                gpio.write(thisP.motorPins[i], val2Write[i], function(){})
+            };
+            break;
+        default:
+            console.log("invalid status");
+    }
 }
