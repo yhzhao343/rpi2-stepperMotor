b11a874937601f8432435397628ac532637eb4ac
yhzhao343 <yhzhao343@gmail.com>
2016-02-10 23:39:35 -0500

    f

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var gpio = require('rpi-gpio');
 
-gpio.setup(7, gpio.DIR_OUT, write);
+gpio.setup(11, gpio.DIR_OUT, write);
 
 function write() {
-    gpio.write(7, true, function(err) {
+    gpio.write(11, true, function(err) {
         if (err) throw err;
         console.log('Written to pin');
     });
