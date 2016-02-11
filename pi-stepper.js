c175512fb1273817cd6867566cf19cf1ea60da2e
yhzhao343 <yhzhao343@gmail.com>
2016-02-10 23:48:36 -0500

    f

--- pi-stepper.js
+++ pi-stepper.js
@@ -13,7 +13,7 @@ function Motor(motorPins) {
     var motorPins = motorPins;
     var runStatus = 1;
     var cycleNdx = 0;
-    var velocity = 10;
+    var velocity = 50;
     var lastState = null;
 
     this.getVelocity = function() {
