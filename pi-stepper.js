b49ba9d327663afbcb4b49d28704fb284ea5473b
yhzhao343 <yhzhao343@gmail.com>
2016-02-11 19:34:51 -0500

    fix merge

--- pi-stepper.js
+++ pi-stepper.js
@@ -13,7 +13,7 @@ function Motor(motorPins) {
     var motorPins = motorPins;
     var runStatus = 1;
     var cycleNdx = 0;
-    var velocity = 10;
+    var velocity = 1;
     var lastState = Q();
 
     this.newState = function(newState) {
